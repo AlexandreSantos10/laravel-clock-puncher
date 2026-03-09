1ae74654e0b4f6fe989160b7ca6e5a9e91c34bc2
AlexandreSantos10 <alexandrenavalsantos@gmail.com>
2026-03-09 15:56:26 +0000

    componentes-atualizados

--- example-app/resources/views/user/logs.blade.php
+++ example-app/resources/views/user/logs.blade.php
@@ -39,11 +39,7 @@
                                     </div>
                                     <div class="py-2">
                                         <a href="/user/logs">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="size-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
-                                            </svg>
+                                             <x-refresh-icon/>
                                         </a>
                                     </div>
 
@@ -69,11 +65,7 @@
                                 </select>
                             </div>
                             <div>
-                                <x-secondary-app-button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="size-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
-                                    </svg>
+                                <x-secondary-app-button><x-export-icon/>
                                 </x-secondary-app-button>
                             </div>
                         </div>
