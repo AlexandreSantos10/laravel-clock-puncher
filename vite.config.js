d94fb2cede6bd78175e435660b46733a470832a9
Bolinhaaaa <rodrigo.duarte@mindshaker.com>
2026-04-15 15:57:11 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,17 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        ...(process.env.DDEV_PRIMARY_URL_WITHOUT_PORT
+            ? {
+                  origin: `${process.env.DDEV_PRIMARY_URL_WITHOUT_PORT}:5173`,
+                  cors: {
+                      origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+                  },
+              }
+            : {}),
+    },
 });
